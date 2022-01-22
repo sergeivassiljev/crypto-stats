e0a52e66f4860ac3fb64128e0008d4d8289ec7ca
metro <servas@ttu.ee>
2022-01-23 00:24:20 +0200

    Saving to local storage

--- src/App.vue
+++ src/App.vue
@@ -138,7 +138,37 @@ export default {
       graph: [],
     };
   },
+
+  created() {
+    const tickersData = localStorage.getItem("cryptonomicon-list");
+
+    if(tickersData){
+    this.tickers = JSON.parse(tickersData);
+    this.tickers.forEach(ticker=>{
+      this.subscribeToUpdates(ticker.name);
+    })
+    }
+  },
+
   methods: {
+
+    subscribeToUpdates(tickerName){
+       setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=2a4d8ced1ab2b35b2e2f871c12dbb97793b0bff6dda269142fccdabc81281ce7`
+        );
+        const data = await f.json();
+
+        // currentTicker.price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(3);
+        this.tickers.find((t) => t.name === tickerName).price =
+          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(3);
+
+        if (this.sel?.name === tickerName) {
+          this.graph.push(data.USD);
+        }
+      }, 5000);
+    },
+
     add() {
       const currentTicker = {
         name: this.ticker,
@@ -146,21 +176,12 @@ export default {
       };
 
       this.tickers.push(currentTicker);
-      setInterval(async () => {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=2a4d8ced1ab2b35b2e2f871c12dbb97793b0bff6dda269142fccdabc81281ce7`
-        );
-        const data = await f.json();
 
-        // currentTicker.price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(3);
-        this.tickers.find((t) => t.name === currentTicker.name).price =
-          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(3);
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
 
-        if (this.sel?.name === currentTicker.name) {
-          this.graph.push(data.USD);
-        }
-      }, 5000);
-      this.ticker = "";
+      this.subscribeToUpdates(currentTicker.name);
+     
+      // this.ticker = "";
     },
 
     normalizeGraph() {
